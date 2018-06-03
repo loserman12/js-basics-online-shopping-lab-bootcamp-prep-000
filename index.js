105cd34de2737d4a1ab9164df521f574415bec35
ralphcuello1016@gmail.com <ralphcuello1016@gmail.com>
2018-06-03 21:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ function viewCart() {
     let lastItem = cartier.pop();
     // var lastItem will  *.pop* remove the last element in the the cartier array and return the element taken.
     return `In your cart, you have ${cartier.join(', ')}, and ${lastItem}.`
-    
-  // 
+     // 
   }
   }
  
